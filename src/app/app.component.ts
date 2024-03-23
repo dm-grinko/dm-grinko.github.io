06d90cf30ae103d98ada78095bee0dfd00d5b065
Dmitry Grinko <dm.grinko@gmail.com>
2024-03-23 11:43:29 -0700

    update resume

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,28 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'dmitrygrinko.com';
+  workExperience = [
+    {
+      company: 'Sogeti USA, LLC',
+      dates: '06/2022 - 03/2024',
+      position: 'Manager',
+      responsibilities: [
+        'Led, mentored, and trained team members at Prometric...',
+        'Collaborated with Kirkland & Ellis to successfully migrate infrastructure...'
+      ]
+    },
+    // Add more work experiences
+  ];
+
+  certifications = [
+    {
+      name: 'AWS Certified Solutions Architect - Associate',
+      credentialId: 'Z93LQD41SFB1Q1WM',
+      dates: '07/2024 - 07/2025'
+    },
+    // Add more certifications
+  ];
+
+  cloudSkills = 'Amazon Web Services (AWS), Microsoft Azure, Terraform, ...';
+  backendSkills = 'Node.js, Express.js, MongoDB, ...';
 }
